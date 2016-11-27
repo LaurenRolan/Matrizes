19b94bdfb136a276345779126893ae71a8ea6fbb
LaurenRolan <lauren_rolan@yahoo.com.br>
2016-11-27 18:10:36 -0200

    funcionando parcialmente

--- MatrixMult.vhd
+++ MatrixMult.vhd
@@ -33,8 +33,7 @@ use ieee.std_logic_unsigned.all;
 
 entity MatrixMult is
     Port ( clk : in  STD_LOGIC;
-           rst : in  STD_LOGIC;
-			  s: STD_LOGIC_VECTOR(7 downto 0)
+           rst : in  STD_LOGIC
 			  );
 end MatrixMult;
 
@@ -42,53 +41,18 @@ architecture Behavioral of MatrixMult is
 
 type STATE is (s0a, s0, s1, s2);
 signal estado, proxEstado: STATE;
-type matriz is array(3 DOWNTO 0, 3 DOWNTO 0) OF STD_LOGIC_VECTOR(3 DOWNTO 0);
+type matriz is array(0 TO 3, 0 TO 3) OF STD_LOGIC_VECTOR(3 DOWNTO 0);
 signal a, b : matriz;
 signal x,y,i,j: integer :=0; 
 signal rowB,colB: integer :=0; 
-signal wea, web :STD_LOGIC_VECTOR(0 DOWNTO 0);
-signal addra, addrb: STD_LOGIC_VECTOR(4 DOWNTO 0);
-signal dina, douta, dinb, doutb : STD_LOGIC_VECTOR(3 DOWNTO 0);
+signal wea, web :STD_LOGIC_VECTOR(0 DOWNTO 0) := "0";
+signal addra, addrb: STD_LOGIC_VECTOR(4 DOWNTO 0); -- era 4 downto 0
+signal dina, douta, dinb, doutb : STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000"; -- era 4
 signal ldaA, ldaB, sel, incI, incJ, incX, incY: STD_LOGIC;
-signal incEndA, incEndB, zeraI, zeraJ, zeraX, zeraY, compEndA, compJ: STD_LOGIC;
+signal incEndA, incEndB, zeraI, zeraJ, zeraX, zeraY, compEndA, compJ: STD_LOGIC := '0';
 signal c,d,e,f: integer :=0;
-COMPONENT MemEntrada
-  PORT (
-    clka : IN STD_LOGIC;
-    wea : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
-    addra : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
-    dina : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-    douta : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
-  );
-  END COMPONENT;
 
-COMPONENT memB
-  PORT (
-    clka : IN STD_LOGIC;
-    wea : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
-    addra : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
-    dina : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-    douta : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
-  );
-  END COMPONENT;
-  
 begin
-	memA : MemEntrada
-  PORT MAP (
-    clka => clk,
-    wea => wea,
-    addra => addra,
-    dina => dina,
-    douta => douta
-  );
-	memoB : memB
-  PORT MAP (
-    clka => clk,
-    wea => web,
-    addra => addrb,
-    dina => dinb,
-    douta => doutb
-  );
  -- FSM 
   process(clk, rst)
   begin
@@ -99,11 +63,12 @@ begin
 	end if;
   end process;
   
-  process(clk, estado, proxEstado)
+  process(clk, estado, proxEstado, compJ, compEndA)
   begin
 	case estado is
 		when s0a=> incY <='0'; incJ <='0'; incEndA<= '0'; incEndB <= '0'; 
 						zeraY <= '0'; zeraJ <='0'; incI<='0'; incX <='0'; proxEstado<= s0;
+						ldaA <= '0'; ldaB <= '0';
 						
 		when s0 => ldaA <='1'; ldaB <='1'; sel <= '1'; proxEstado<= s1;
 		
@@ -124,21 +89,22 @@ begin
 	end case;
   end process;
 -- Comparadores
-	compJ <= '1' when (j > 3) else '0'; --?????????
-	compEndA <= '1' when (addrA > "10000") else '0'; 
+	compJ <= '1' when (j = 3) else '0'; --?????????
+	compEndA <= '1' when (addrA = "10000") else '0'; -- era > "10000" same shit
 	
 --regA	
 		process(clk, rst)
 		begin
-			if(rst = '0') then 
+			if(rst = '1') then  -- funciona just fine
 				for e in 3 downto 0 loop
 					for f in 3 downto 0 loop
 						a(e,f) <= "0000";
 					end loop;
 				end loop;
+				-- até aqui funfa
 			elsif (rising_edge(clk)) then
 				if(ldaA = '1') then
-					a(i,j) <= doutb;
+					a(i,j) <= douta; -- era só douta
 				end if;
 			end if;
 		end process;
@@ -149,7 +115,7 @@ begin
 -- regB
 		process(clk, rst)
 		begin
-			if(rst = '0') then 
+			if(rst = '1') then 
 				for c in 3 downto 0 loop
 					for d in 3 downto 0 loop
 						b(c,d) <= "0000";
@@ -182,7 +148,7 @@ begin
 		if(rst = '1') then
 			y <= 0;
 		elsif(rising_edge(clk)) then
-			if incy = '1' then
+			if incy = '1' and y <= 3 then
 				y<= y +1;
 			elsif zeray = '1' then
 				y<= 0;
@@ -210,7 +176,7 @@ begin
 		if(rst = '1') then
 			j <= 0;
 		elsif(rising_edge(clk)) then
-			if incj = '1' then
+			if incj = '1' and j <= 3 then
 				j<= j +1;
 			elsif zeraj = '1' then
 				j<= 0;
@@ -225,7 +191,9 @@ begin
 			addrA <= "00001";
 		elsif(rising_edge(clk)) then
 			if incEndA = '1' then
-				addra<= addra +1;
+				addrA<= addrA + '1';
+				--if addrA = "10000" then compEndA <= '1';
+				--else compEndA <= '0'; end if;
 			end if;
 		end if;
 	end process;
@@ -236,18 +204,13 @@ begin
 			addrB <= "00001";
 		elsif(rising_edge(clk)) then
 			if incEndB = '1' then
-				addrB<= addrB +1;
+				addrB<= addrB + '1';
 			end if;
 		end if;
 	end process;
-
-
-		
-		
-		
+	
+-- Memórias
+MEMA: entity work.memoA port map (clk, wea, addrA, dina, douta);
+MEMB: entity work.memoA port map (clk, web, addrB, dinb, doutb);
 		
 end Behavioral;
-
-
-	
-	
